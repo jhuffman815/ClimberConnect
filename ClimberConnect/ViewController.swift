c1ab27ff2ef185ba12e1411026c303b8b023d1b9
Jewell <jhuffman815@gmail.com>
2020-03-02 23:42:16 -0500

    Initial Commit

---
+++ ClimberConnect/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ClimberConnect
+//
+//  Created by Jewell Huffman on 3/2/20.
+//  Copyright © 2020 Jewell Huffman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ClimberConnectTests/ClimberConnectTests.swift
@@ -0,0 +1,34 @@
+//
+//  ClimberConnectTests.swift
+//  ClimberConnectTests
+//
+//  Created by Jewell Huffman on 3/2/20.
+//  Copyright © 2020 Jewell Huffman. All rights reserved.
+//
+
+import XCTest
+@testable import ClimberConnect
+
+class ClimberConnectTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
