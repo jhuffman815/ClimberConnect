bb4b2d95047e2bc8558ac08fbb0e3b7ee81ffdd4
Jewell <jhuffman815@gmail.com>
2020-03-03 20:29:46 -0500

    Changed layout to tableviewcontroller

---
+++ ClimberConnect/Member.swift
@@ -0,0 +1,21 @@
+//
+//  Member.swift
+//  ClimberConnect
+//
+//  Created by Jewell Huffman on 3/3/20.
+//  Copyright © 2020 Jewell Huffman. All rights reserved.
+//
+
+import SwiftUI
+
+struct Member: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Member_Previews: PreviewProvider {
+    static var previews: some View {
+        Member()
+    }
+}
